48d7b395e52bf8ab7381569518615b05a9d1433f
numocityadmin <85339269+numocityadmin@users.noreply.github.com>
2022-09-13 15:46:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def count_batteries_by_usage(cycles):
 def test_bucketing_by_number_of_cycles():
   print("Counting batteries by usage cycles...\n");
   counts = count_batteries_by_usage([-1,-2,100, 300, 500, 600, 900, 1000])
-  assert(counts["lowCount"] == 2)
+  assert(counts["lowCount"] == 1)
   assert(counts["mediumCount"] == 3)
   assert(counts["highCount"] == 1)
   print("Done counting :)")
